914d17ef3f1c265fd1f22be6811f2ffc61c01c91
Dehou Kong <124kdaniel@gmail.com>
2022-09-18 21:36:38 -0400

    revert back to deploy.sh and remove action file

--- src/router.ts
+++ src/router.ts
@@ -3,14 +3,13 @@ import DeckAnalyzer from './components/DeckAnalyzer.vue'
 import CardFetcher from './components/CardFetcher.vue'
 import Home from './components/Home.vue'
 
-const repoName = '/vue-mtg-tools'
-
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: repoName, name:'home', component: Home },
-    { path: repoName+"/deckanalyzer", name:'deckanalyzer', component: DeckAnalyzer },
-    { path: repoName+"/cardfetcher", name:'cardfetcher', component: CardFetcher }
+    { path: "/", redirect: { name: 'Home' }},
+    { path: "/vue-mtg-tools", name:'home', component: Home },
+    { path: "/deckanalyzer", name:'deckanalyzer', component: DeckAnalyzer },
+    { path: "/cardfetcher", name:'cardfetcher', component: CardFetcher }
   ]
 });
 export default router;
