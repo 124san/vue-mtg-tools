0351abb4aefbe5ecfd2a31c28ea8f48b4069e259
Dehou Kong <124kdaniel@gmail.com>
2022-09-18 21:25:26 -0400

    update router paths

--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,14 @@ import DeckAnalyzer from './components/DeckAnalyzer.vue'
 import CardFetcher from './components/CardFetcher.vue'
 import Home from './components/Home.vue'
 
+const repoName = '/vue-mtg-tools'
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", name:'home', component: Home },
-    { path: "/deckanalyzer", name:'deckanalyzer', component: DeckAnalyzer },
-    { path: "/cardfetcher", name:'cardfetcher', component: CardFetcher }
+    { path: repoName, name:'home', component: Home },
+    { path: repoName+"/deckanalyzer", name:'deckanalyzer', component: DeckAnalyzer },
+    { path: repoName+"/cardfetcher", name:'cardfetcher', component: CardFetcher }
   ]
 });
 export default router;
