52df00a99407b12d3c9b4fe4ba30188ed30da43e
Dehou Kong <124kdaniel@gmail.com>
2022-09-19 15:25:23 -0400

    fix router x2

--- src/router.ts
+++ src/router.ts
@@ -7,12 +7,12 @@ const repoName = '/vue-mtg-tools'
 const router = createRouter({
   history: createWebHistory(repoName+"/"),
   routes: [
-    { path: "/", redirect: { name: 'home' }},
-    { path: repoName, name:'home', component: Home },
-    { path: "/deckanalyzer", redirect: { name: 'deckanalyzer' }},
-    { path: repoName+"/deckanalyzer", name:'deckanalyzer', component: DeckAnalyzer },
-    { path: "/cardfetcher", redirect: { name: 'cardfetcher' }},
-    { path: repoName+"/cardfetcher", name:'cardfetcher', component: CardFetcher }
+    // { path: "/", redirect: { name: 'home' }},
+    { path: "/", name:'home', component: Home },
+    // { path: "/deckanalyzer", redirect: { name: 'deckanalyzer' }},
+    { path: "/deckanalyzer", name:'deckanalyzer', component: DeckAnalyzer },
+    // { path: "/cardfetcher", redirect: { name: 'cardfetcher' }},
+    { path: "/cardfetcher", name:'cardfetcher', component: CardFetcher }
   ]
 });
 export default router;
